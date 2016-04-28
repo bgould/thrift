758362472665eb1cbc2df99b93dca93ba1dd39c3
BCG <bgould@users.noreply.github.com>
2016-04-28 09:43:07 -0400

    Fixes for Windows

--- contrib/nestedvm/src/main/java/org/apache/thrift/compiler/NativeThriftCompiler.java
+++ contrib/nestedvm/src/main/java/org/apache/thrift/compiler/NativeThriftCompiler.java
@@ -21,7 +21,7 @@ package org.apache.thrift.compiler;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.Scanner;
+import java.io.OutputStream;
 
 /**
  * Implementation of {@link ThriftCompiler} that uses a native Thrift executable.
@@ -46,51 +46,36 @@ public class NativeThriftCompiler extends ThriftCompiler {
     String errString = null;
     int exit = Integer.MIN_VALUE;
     boolean interrupted = false;
-    boolean processDestroyed = false;
     try {
-//      final ByteArrayOutputStream out = new ByteArrayOutputStream();
-//      final ByteArrayOutputStream err = new ByteArrayOutputStream();
+      final ByteArrayOutputStream out = new ByteArrayOutputStream();
+      final ByteArrayOutputStream err = new ByteArrayOutputStream();
       final Process process = Runtime.getRuntime().exec(proc_args);
-//      final byte[] buffer = new byte[1024];
+      final InputStream outstream = process.getInputStream();
+      final InputStream errstream = process.getErrorStream();
+      final byte[] buffer = new byte[1024];
       try {
-//        outString = asString(process.getInputStream());
-//        errString = asString(process.getErrorStream());
-        /*
-        for (int e = 0, o = 0; (e > -1 && o > -1) ; ) {
-          if (e > -1) {
-            e = process.getErrorStream().read(buffer);
-            if (e > -1) {
-              err.write(buffer, 0, e);
-            }
-          }
-          if (o > -1) {
-            o = process.getInputStream().read(buffer);
-            if (o > -1) {
-              out.write(buffer, 0, o);
+        for (boolean exited = false; !exited; ) {
+          final int readFromOut = nonBlockingCopy(outstream, out, buffer);
+          final int readFromErr = nonBlockingCopy(errstream, err, buffer);
+          if (readFromOut < 1 && readFromErr < 1) {
+            try {
+              exit = process.exitValue();
+              exited = true;
+            } catch (IllegalThreadStateException e) {
+              // the process has not exited yet, so loop again
+              try { Thread.sleep(1); } catch (InterruptedException e2) {}
             }
           }
         }
-        outString = out.toString();
-        errString = out.toString();
-        */
-        final StreamConsumer outEater = new StreamConsumer(process.getInputStream());
-        final StreamConsumer errEater = new StreamConsumer(process.getErrorStream());
-        outEater.start();
-        errEater.start();
-        exit = process.waitFor();
-        outString = outEater.getString();
-        errString = errEater.getString();
-        process.destroy();
-        processDestroyed = true;
-//        System.out.println("err: " + errString);
-//        System.out.println("out: " + outString);
-      } catch (InterruptedException e) {
-        interrupted = true;
+        blockingCopy(outstream, out, buffer);
+        blockingCopy(errstream, err, buffer);
       } finally {
-        if (!processDestroyed) {
-          process.destroy();
-        }
+        try { outstream.close(); } catch (IOException e) {}
+        try { errstream.close(); } catch (IOException e) {}
+        process.destroy();
       }
+      outString = out.toString();
+      errString = err.toString();
       return new ExecutionResult(exit, interrupted, outString, errString, null);
     } catch (IOException e) {
       throw new ThriftCompilerException(e);
@@ -101,53 +86,34 @@ public class NativeThriftCompiler extends ThriftCompiler {
     return executable;
   }
 
-  private static String asString(final InputStream in) {
-    try (final Scanner s = new Scanner(in)) {
-      return s.useDelimiter("\\A").hasNext() ? s.next() : "";
-    }
-  }
-
   @Override
   public boolean isNativeExecutable() {
     return true;
   }
 
-  static class StreamConsumer extends Thread {
-    final InputStream in;
-    String output;
-    boolean done = false;
-    Throwable t;
-    StreamConsumer(InputStream in) {
-      this.in = in;
-    }
-    @Override
-    public void run() {
-      final byte[] buffer = new byte[1024];
-      final ByteArrayOutputStream out = new ByteArrayOutputStream();
-      try {
-        for (int n; (n = in.read(buffer)) > -1; ) {
-          out.write(buffer, 0, n);
-        }
-        output = out.toString();
-      } catch (Throwable e) {
-        t = e;
-      } finally {
-        done = true;
+  private final int nonBlockingCopy(
+        final InputStream in, final OutputStream out, final byte[] buf
+      ) throws IOException {
+    final int bytesAvailable = in.available();
+    if (bytesAvailable > 0) {
+      final int bytesRead = in.read(buf, 0, Math.min(bytesAvailable, 1024));
+      if (bytesRead > 0) {
+        out.write(buf, 0, bytesRead);
       }
-    }
-    public String getString() {
-      while (!done) {
-        try {
-          Thread.sleep(1);
-        } catch (InterruptedException e) {
-          break;
-        }
-      }
-      if (t != null) {
-        throw new RuntimeException(t);
-      } else {
-        return output;
-      }
+      return bytesRead;
+    } else {
+      return 0;
     }
   }
+
+  private final int blockingCopy(
+      final InputStream in, final OutputStream out, final byte[] buf
+    ) throws IOException {
+    int total = 0;
+    for (int n; (n = in.read(buf)) > -1; total += n) {
+      out.write(buf, 0, n);
+    }
+    return total;
+  }
+
 }
